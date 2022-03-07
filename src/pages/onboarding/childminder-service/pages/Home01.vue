92ef54bca7360cef686b5fe7f52440ec7d6cb9f8
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-07 15:50:09 +1100

    [vue] container page updated to include newly created home page

---
+++ src/pages/onboarding/childminder-service/pages/Home01.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="bg-white h-screen">
+		<main>
+			<div class="min-h-full flex flex-col mx-4">
+				<h2 class="my-6 text-left text-2xl">Welcome, Brittany</h2>
+			</div>
+		</main>
+	</div>
+</template>
