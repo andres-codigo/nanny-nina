b4c3c66eff17dc9de193856d01905872f05d8bc9
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-08 12:35:56 +1100

    [vue] DOM/classes for cms onboarding home page

--- src/pages/onboarding/childminder-service/pages/Home01.vue
+++ src/pages/onboarding/childminder-service/pages/Home01.vue
@@ -1,30 +1,44 @@
+<script setup>
+import Image from '../../../../components/carousel/Image.vue'
+</script>
+
 <template>
 	<div class="bg-white w-full h-screen">
 		<main>
-			<div class="min-h-full flex flex-col mx-4">
-				<h2 class="my-6 text-left text-2xl">Welcome, Brittany</h2>
-				<div class="flex flex-row">
-					<div class="basis-1/2">
-						<h1 class="text-gray font-normal antialiased">Explore Jobs</h1>
+			<div class="min-h-full flex flex-col mx-4 mt-10">
+				<div class="flex flex-row mb-4">
+					<div class="basis-1/2 ml-4">
+						<span class="inline-block text-xl">Hello, Brittany</span>
+						<span class="inline-block text-lg text-gray antialiased">Explore Jobs</span>
 					</div>
-					<div class="basis-1/2 pr-6 text-gray text-right">
-						<span class="text-black font-light tracking-tighter">30% Ready to work</span>
+					<div class="basis-1/2 mr-4 relative">
+						<div class="absolute -bottom-1 right-0 ">
+							<span class="inline-block text-sm text-black font-light antialiased">30% Ready to work</span>
+							<div class="bg-gray-500 rounded-full overflow-hidden">
+								<div class="h-2 bg-indigo-600 rounded-full" style="width: 30%"></div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<router-link to="/" class="bg-white h-80 drop-shadow-md rounded-3xl focus:outline-none focus:ring-2 focus:ring-offset-2">
+				<a href="/" class="h-80 bg-white drop-shadow-md rounded-3xl focus:outline-none focus:ring-2 focus:ring-offset-2">
 					<div class="flex flex-row">
-						<div class="flex grow h-40 bg-indigo-200 rounded-t-3xl">&nbsp;</div>
-					</div>
-					<div class="flex flex-row">
-						<div class="basis-1/2 pt-6 pl-6">
-							<h1 class="text-3xl font-medium antialiased">Nanny</h1>
-						</div>
-						<div class="basis-1/2 pt-6 pr-6 text-gray text-right">
-							<span class="font-light tracking-tighter align-middle">0/5 steps completed</span>
+						<div class="flex grow h-40 bg-indigo-200 rounded-t-3xl overflow-hidden">
+							<!--
+								Image component for svg here
+								<Image src="*.svg" />
+							-->
 						</div>
 					</div>
-					<p class="text-lg font-light mt-6 px-6 leading-5">Work on regular fixed schedule, returning days per week</p>
-				</router-link>
+					<div class="flex flex-row">
+						<div class="basis-1/2 mt-4 ml-4">
+							<h1 class="antialiased text-xl">Nanny</h1>
+						</div>
+						<div class="basis-1/2 mt-4 mr-4 text-right">
+							<span class="text-xs text-gray font-light">0/5 steps completed</span>
+						</div>
+					</div>
+					<p class="text-base font-light leading-5 mt-4 mx-4">Work on regular fixed schedule, returning days per week</p>
+				</a>
 			</div>
 		</main>
 	</div>
