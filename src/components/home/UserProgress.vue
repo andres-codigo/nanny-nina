b47d89164cc1be4a6dacd43c0b0a70bfc39968d1
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-08 12:57:39 +1100

    [vue] note for developers for background colour class needing to be eventually updated

--- src/components/home/UserProgress.vue
+++ src/components/home/UserProgress.vue
@@ -7,6 +7,7 @@
 		<div class="basis-1/2 mr-4 relative">
 			<div class="absolute -bottom-1 right-0 ">
 				<span class="inline-block text-sm text-black font-light antialiased">30% Ready to work</span>
+				<!-- bg-gray-500 needs to be brought down to 200... temporary value as need hex values for other colours ranges -->
 				<div class="bg-gray-500 rounded-full overflow-hidden">
 					<div class="h-2 bg-indigo-600 rounded-full" style="width: 30%"></div>
 				</div>
