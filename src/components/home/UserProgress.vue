1b5c42a5745103a6117fc9e12886efd4202dafe0
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-08 12:55:54 +1100

    [vue] UserProgress and HomeCard components created/Home01 page updated/margin bottom class added for multiple HomeCard's

---
+++ src/components/home/UserProgress.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="flex flex-row mb-4">
+		<div class="basis-1/2 ml-4">
+			<span class="inline-block text-xl">Hello, Brittany</span>
+			<span class="inline-block text-lg text-gray antialiased">Explore Jobs</span>
+		</div>
+		<div class="basis-1/2 mr-4 relative">
+			<div class="absolute -bottom-1 right-0 ">
+				<span class="inline-block text-sm text-black font-light antialiased">30% Ready to work</span>
+				<div class="bg-gray-500 rounded-full overflow-hidden">
+					<div class="h-2 bg-indigo-600 rounded-full" style="width: 30%"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
--- src/pages/onboarding/childminder-service/pages/Home01.vue
+++ src/pages/onboarding/childminder-service/pages/Home01.vue
@@ -1,45 +1,16 @@
 <script setup>
-import Image from '../../../../components/carousel/Image.vue'
+import UserProgress from '../../../../components/home/UserProgress.vue'
+import HomeCard from '../../../../components/cards/HomeCard.vue'
 </script>
 
 <template>
 	<div class="bg-white w-full h-screen">
 		<main>
 			<div class="min-h-full flex flex-col mx-4 mt-10">
-				<div class="flex flex-row mb-4">
-					<div class="basis-1/2 ml-4">
-						<span class="inline-block text-xl">Hello, Brittany</span>
-						<span class="inline-block text-lg text-gray antialiased">Explore Jobs</span>
-					</div>
-					<div class="basis-1/2 mr-4 relative">
-						<div class="absolute -bottom-1 right-0 ">
-							<span class="inline-block text-sm text-black font-light antialiased">30% Ready to work</span>
-							<div class="bg-gray-500 rounded-full overflow-hidden">
-								<div class="h-2 bg-indigo-600 rounded-full" style="width: 30%"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<a href="/" class="h-80 bg-white drop-shadow-md rounded-3xl focus:outline-none focus:ring-2 focus:ring-offset-2">
-					<div class="flex flex-row">
-						<div class="flex grow h-40 bg-indigo-200 rounded-t-3xl overflow-hidden">
-							<!--
-								Image component for svg here
-								<Image src="*.svg" />
-								* remove background colour in div as visual placeholder
-							-->
-						</div>
-					</div>
-					<div class="flex flex-row">
-						<div class="basis-1/2 mt-4 ml-4">
-							<h1 class="antialiased text-xl">Nanny</h1>
-						</div>
-						<div class="basis-1/2 mt-4 mr-4 text-right">
-							<span class="text-xs text-gray font-light">0/5 steps completed</span>
-						</div>
-					</div>
-					<p class="text-base font-light leading-5 mt-4 mx-4">Work on regular fixed schedule, returning days per week</p>
-				</a>
+				<UserProgress />
+				<HomeCard tempBackgroundColour="bg-pink-100" user="Nanny" content="Work on regular fixed schedule, returning days per week" />
+				<HomeCard tempBackgroundColour="bg-indigo-100" user="Au Pair" content="Experience a new culture with a host family abroad as a caregiver" />
+				<HomeCard tempBackgroundColour="bg-emerald-100" user="Childminder" content="Work as a pro-nanny" />
 			</div>
 		</main>
 	</div>
