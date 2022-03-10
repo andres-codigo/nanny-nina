627c4b4c6edc858203f6bc4c3660bd41882469db
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-10 11:03:41 +1100

    [vue] DOM updates based on design update/removed of incorrect markup

--- src/components/media/single-upload/SinglePhotoUpload.vue
+++ src/components/media/single-upload/SinglePhotoUpload.vue
@@ -4,7 +4,7 @@ import SingleUploadItem from './SingleUploadItem.vue'
 
 <template>
 	<h2 class="mb-2 text-left text-2xl font-normal text-gray-900">Let's put a face to your name</h2>
-	<h3	h3 class="font-light">Preferably a smiling picture, if you have a pet or kid in it even better!</h3>
+	<h3	class="font-light">Preferably a smiling picture, if you have a pet or kid in it even better!</h3>
 
 	<div class="mt-10">
 		<SingleUploadItem />
--- src/pages/host/nanny/pages/BioForm01.vue
+++ src/pages/host/nanny/pages/BioForm01.vue
@@ -12,21 +12,8 @@ import ButtonPrimary from '../../../../components/buttons/ButtonPrimary.vue'
 			<div class="flex flex-col pt-6 mx-4">
 				<form action="#" method="POST">
 					<h2 class="my-6 text-left text-2xl font-normal text-gray-900">Let's get to know each other</h2>
-					<div class="h-28">
-						<h3 class="text-lg font-light">What is your gender?</h3>
-						<div class="w-1/2 mt-4">
-							<SelectMenu text="Gender" />
-						</div>
-					</div>
 
-					<div class="mt-8 h-28">
-						<h3 class="text-lg font-light">When is your birthday?</h3>
-						<div class="w-1/2 mt-4">
-							<InputField text="Date of Birth" placeholder="DD/MM/YYY" />
-						</div>
-					</div>
-
-					<div class="mt-16">
+					<div class="my-16">
 						<h3 class="text-lg font-light">When do you live?</h3>
 						<div class="mt-4 grid grid-cols-2 gap-y-6 gap-x-2">
 							<div><InputField text="Country" /></div>
@@ -41,11 +28,6 @@ import ButtonPrimary from '../../../../components/buttons/ButtonPrimary.vue'
 							<div><InputField text="Province" /></div>
 							<div><InputField text="State/City" /></div>
 						</div>
-					</div>
-
-					<div class="mt-16">
-						<h3 class="text-lg font-light">How many children do you have?</h3>
-
 					</div>
 
 					<div class="mt-10">
