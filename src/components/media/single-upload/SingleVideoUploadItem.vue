91ca8c52e2bc2542088662d8b0a1e5b47bd0b43c
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-15 21:01:30 +1100

    [vue] all upload scenario DOM updated based on feedback

--- src/components/media/single-upload/SingleVideoUploadItem.vue
+++ src/components/media/single-upload/SingleVideoUploadItem.vue
@@ -1,15 +1,17 @@
 <template>
 	<div class="flex items-center justify-center mb-6">
-		<span
+		<button
 			class="
 				flex items-center justify-center
 				relative
 				h-56 w-56
 				rounded-full
 				bg-indigo-100
+				hover:border-transparent hover:text-slate-700 hover:bg-slate-200
+				focus:outline-none focus:ring-2 focus:ring-offset-2
 			">
 			<VideoCameraIcon class="w-32 h-32 text-indigo-400" aria-hidden="true" />
-			<button
+			<span
 				class="
 					flex items-center justify-center
 					absolute bottom-6 right-0
@@ -17,12 +19,10 @@
 					rounded-full drop-shadow-lg
 					text-slate-800
 					bg-slate-100
-					hover:border-transparent hover:text-slate-700 hover:bg-slate-200
-					focus:outline-none focus:ring-2 focus:ring-offset-2
 				">
 				<PlusIcon class="w-8 h-8" aria-hidden="true" />
-			</button>
-		</span>
+			</span>
+		</button>
 	</div>
 	<p class="text-sm font-light text-center text-gray-500">Size limit 8MB</p>
 </template>
