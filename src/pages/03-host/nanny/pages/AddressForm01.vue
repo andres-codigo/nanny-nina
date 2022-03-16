ff77fc0e0a3d360c459130d99169075f5822df54
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-16 17:20:25 +1100

    [vue] top host > nanny > address form DOM and classes updated

--- src/pages/03-host/nanny/pages/AddressForm01.vue
+++ src/pages/03-host/nanny/pages/AddressForm01.vue
@@ -6,15 +6,15 @@ import ButtonPrimary from '../../../../components/buttons/ButtonPrimary.vue'
 </script>
 
 <template>
-	<div class="h-full">
+	<div class="w-full h-full">
 		<TopNavigation routerLinkPath="/dashboard" text="Basic Information" />
 		<main>
 			<div class="flex flex-col pt-6 mx-4">
 				<form action="#" method="POST">
-					<h2 class="my-6 text-left text-2xl font-normal text-gray-900">Let's get to know each other</h2>
+					<h5 class="my-6 text-left text-2xl font-normal text-gray-900">Let's get to know each other</h5>
 
 					<div class="my-16">
-						<h3 class="text-lg font-light">When do you live?</h3>
+						<span class="inline-block font-light">When do you live?</span>
 						<div class="mt-4 grid grid-cols-2 gap-y-6 gap-x-2">
 							<div><InputField text="Country" /></div>
 							<div><InputField text="Postcode" /></div>
@@ -32,7 +32,7 @@ import ButtonPrimary from '../../../../components/buttons/ButtonPrimary.vue'
 
 					<div class="mt-10">
 						<div class="my-6">
-							<ButtonPrimary text="Next" />
+							<ButtonPrimary text="Save &amp; Continue" />
 						</div>
 					</div>
 				</form>
