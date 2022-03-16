d3b6661f93d1a5b8ea7099ed827554f637aa44f5
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-16 17:15:18 +1100

    [vue] top navigation header class updated

--- src/components/navigation/TopNavigation.vue
+++ src/components/navigation/TopNavigation.vue
@@ -4,7 +4,7 @@
 			<div class="flex-none inline-flex justify-center items-center -ml-1.5 w-14 h-14">
 				<router-link :to="routerLinkPath"><ChevronLeftIcon class="w-8 h-8 text-white" aria-hidden="true" /></router-link>
 			</div>
-			<div :class="['grow h-14 inline-flex justify-center items-center text-white', hideHeader ? 'hidden sm:hidden' : '']">{{ text }}</div>
+			<div :class="['grow h-14 inline-flex justify-center items-center text-xl text-white', hideHeader ? 'hidden sm:hidden' : '']">{{ text }}</div>
 			<div class="flex-none inline-flex justify-center items-center w-14 h-14">
 				<QuestionMarkCircleIcon :class="['w-8 h-8 text-green-500', hideIcon ? 'hidden sm:hidden' : '']" aria-hidden="true" />
 			</div>
