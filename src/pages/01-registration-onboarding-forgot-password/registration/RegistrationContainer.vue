960bf20b1588e162edb83bcb5a50d59c3c27b067
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-03-27 14:32:06 +1100

    [components/pages] CheckboxLeftWithContainerAndLabelRight component created and existing checkbox component name and pages updated/SelectMenu classes updated/SignUp page created and DOM added

--- src/pages/01-registration-onboarding-forgot-password/registration/RegistrationContainer.vue
+++ src/pages/01-registration-onboarding-forgot-password/registration/RegistrationContainer.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="flex flex-wrap gap-y-6 sm:gap-x-4 w-screen h-screen">
 		<Welcome01 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
-		<SignUp02 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
+		<SignUpOptions02 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
+		<SignUp03 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
 
 		<VerifyStep01 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
 		<VerifyStep02 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
@@ -13,18 +14,20 @@
 
 <script>
 import Welcome01 from './pages/Welcome01.vue'
-import SignUp02 from './pages/SignUp02.vue'
+import SignUpOptions02 from './pages/SignUpOptions02.vue'
+import SignUp03 from './pages/SignUp03.vue'
 
 import VerifyStep01 from './pages/verification/VerifyStep01.vue'
 import VerifyStep02 from './pages/verification/VerifyStep02.vue'
 import VerifyStep03 from './pages/verification/VerifyStep03.vue'
 
-import Login04 from './pages/Login03.vue'
+import Login04 from './pages/Login04.vue'
 
 export default {
 	components: {
 		Welcome01,
-		SignUp02,
+		SignUpOptions02,
+		SignUp03,
 		VerifyStep01,
 		VerifyStep02,
 		VerifyStep03,
---
+++ src/pages/01-registration-onboarding-forgot-password/registration/pages/SignUp03.vue
@@ -0,0 +1,88 @@
+<template>
+	<div class="w-full">
+		<TopNavigation :hideHeader="true" backgroundColor="bg-white">
+			<template #iconLeft><router-link to="/dashboard"><ChevronLeftIcon class="w-8 h-8" aria-hidden="true" /></router-link></template>
+		</TopNavigation>
+		<main>
+			<div class="flex flex-col justify-center mx-4">
+
+				<h5 class="mb-2 text-left text-header-five font-normal text-gray-900">Get started 100% free</h5>
+				<p class="text-sm font-light">It's free to join, Already have an account? <a href="#" class=" anchor-with-underline">Sign in</a></p>
+
+				<h2 class="mt-8 mb-2 text-left text-header-two font-semibold text-gray-900">Sign up as <span class="text-lilac-500">Parent/Host</span></h2>
+				<p class="font-light">Request Nanny, childminder childcare, Au pair</p>
+
+				<div class="mt-20">
+					<div class="flex flex-col justify-center items-center">
+						<a href="#" class="w-full inline-flex justify-center py-4 px-4 border border-gray-300 rounded-full text-sm font-medium text-gray-900">
+							<Google class="w-5 h-5" />
+							<span class="pl-2">Continue with Google</span>
+						</a>
+					</div>
+
+					<div class="relative my-8">
+						<div class="absolute inset-0 flex items-center">
+							<div class="w-full mx-8 border-t border-gray-300" />
+						</div>
+						<div class="relative flex justify-center text-xs">
+							<span class="px-2 bg-white text-gray-500"> OR </span>
+						</div>
+					</div>
+				</div>
+
+				<form class="w-full" action="#" method="POST">
+
+					<div class="flex flex-col mt-14">
+						<div class="grid grid-cols-2 gap-4 gap-y-6 gap-x-2">
+							<div><SelectMenu text="Gender" /></div>
+							<div><InputField text="Date of Birth" /></div>
+
+							<div><InputField text="First name" /></div>
+							<div><InputField text="Surname" /></div>
+						</div>
+
+						<div class="grid grid-cols-1 gap-4 gap-y-6 gap-x-2 mt-6">
+							<div><InputField text="Email Address" /></div>
+							<div><InputField text="New Password" type="password" value="newpassword" /></div>
+							<div><InputField text="Retype Password" type="text" /></div>
+							<div><SelectMenu text="Country" /></div>
+						</div>
+					</div>
+
+					<div class="flex flex-col justify-center items-center mt-14">
+						<p class="mx-8 text-xs font-medium text-center">Thanks for signing up to Nanny Nina! Based on your sign up, we would love to keep you posted with product news and tips!. You can opt out at any time. See our <a href="#" class="anchor-with-underline">Privacy Policy</a> for more information.</p>
+						<CheckboxLeftWithContainerAndLabelRight text="No, please don’t send any marketing emails or content" />
+						<div class="mt-12 mb-4">
+							<ButtonPrimary text="Sign up" class="py-4 w-button-lg" />
+						</div>
+					</div>
+				</form>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+import TopNavigation from '../../../../components/navigation/TopNavigation.vue'
+import Header from '../../../../components/header/Header.vue'
+import SelectMenu from '../../../../components/form/SelectMenu.vue'
+import InputField from '../../../../components/form/InputField.vue'
+import CheckboxLeftWithContainerAndLabelRight from '../../../../components/form/CheckboxLeftWithContainerAndLabelRight.vue'
+import ButtonPrimary from '../../../../components/buttons/ButtonPrimary.vue'
+import Google from '../../../../assets/svg/other/google.vue'
+
+import { ChevronLeftIcon } from '@heroicons/vue/solid'
+
+export default {
+	components: {
+		TopNavigation,
+		Header,
+		SelectMenu,
+		InputField,
+		CheckboxLeftWithContainerAndLabelRight,
+		ButtonPrimary,
+		Google,
+		ChevronLeftIcon
+	}
+}
+</script>
