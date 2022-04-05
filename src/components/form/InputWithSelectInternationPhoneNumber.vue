faaaa0fd6834cfd59cc6e8cff978de3b73a7da9c
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-05 11:18:37 +1000

    [config.js, components, pages] new width for button tailwind class name added/InputWithSelectInternationPhoneNumber component updated to allow for button to be displayed for verification via sms page/pages updated, renamed and moved and no longer required pages added to newly created _removed-pages folder

--- src/components/form/InputWithSelectInternationPhoneNumber.vue
+++ src/components/form/InputWithSelectInternationPhoneNumber.vue
@@ -1,21 +1,33 @@
 <template>
 	<label for="phone-number" class="block text-sm font-light text-purple-800 pl-4">Phone number</label>
-	<div class="mt-0 flex rounded-md">
+	<div :class="['mt-0 flex rounded-md', showButton ? '' : '']">
 		<label for="international-area-code" class="sr-only">International Area Code</label>
 		<select
 			id="international-area-code"
 			name="international-area-code"
-			class="
-				w-32
-				inline-flex items-center
-				px-3
-				rounded-l-full
-				border border-r-0 border-gray-400
-				focus:outline-none focus:ring-0">
+			:class="['inline-flex items-center px-3 rounded-l-full border border-r-0 border-gray-400 focus:outline-none focus:ring-0', showButton ? 'w-24' : 'w-32']">
 				<option selected>+31 NL</option>
 				<option>+61 AUS</option>
 		</select>
+
+		<div class="relative w-full" v-if="showButton">
+			<input
+				type="text"
+				name="phone-number"
+				id="phone-number"
+				placeholder="Enter number"
+				class="
+					appearance-none block w-[97%]
+					px-3 py-3
+					border border-gray-400
+					rounded-r-full
+					text-sm	text-gray-600
+					focus:outline-none focus:ring-0 focus:border-purple-500">
+			<ButtonPrimary text="Send SMS" class="w-button-input-sms text-xl min-h-[45px] py-3 absolute top-0 left-[55%]" />
+		</div>
+
 		<input
+			v-else
 			type="text"
 			name="phone-number"
 			id="phone-number"
@@ -29,3 +41,19 @@
 				focus:outline-none focus:ring-0 focus:border-purple-500">
 	</div>
 </template>
+
+<script>
+import ButtonPrimary from '../buttons/ButtonPrimary.vue'
+
+export default {
+	components: {
+		ButtonPrimary
+	},
+	props: {
+		showButton: {
+			type: Boolean,
+			default: false
+		}
+	}
+}
+</script>
--- src/pages/01-registration-onboarding-forgot-password/registration/RegistrationContainer.vue
+++ src/pages/01-registration-onboarding-forgot-password/registration/RegistrationContainer.vue
@@ -1,37 +1,27 @@
 <template>
 	<div class="flex flex-wrap gap-y-6 sm:gap-x-4 w-screen h-screen">
-		<Welcome01 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
-		<SignUpOptions02 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
-		<SignUp03 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
-
-		<VerifyStep01 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
-		<VerifyStep02 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
-		<VerifyStep03 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
-
-		<Login04 class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
+		<Welcome class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
+		<SignUpOptions class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
+		<SignUp class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
+		<VerifyNumber class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
+		<Login class="min-w-360-for-dev-not-prod max-w-450-for-dev-not-prod" />
 	</div>
 </template>
 
 <script>
-import Welcome01 from './pages/Welcome01.vue'
-import SignUpOptions02 from './pages/SignUpOptions02.vue'
-import SignUp03 from './pages/SignUp03.vue'
-
-import VerifyStep01 from './pages/verification/VerifyStep01.vue'
-import VerifyStep02 from './pages/verification/VerifyStep02.vue'
-import VerifyStep03 from './pages/verification/VerifyStep03.vue'
-
-import Login04 from './pages/Login04.vue'
+import Welcome from './pages/Welcome.vue'
+import SignUpOptions from './pages/SignUpOptions.vue'
+import SignUp from './pages/SignUp.vue'
+import VerifyNumber from './pages/VerifyNumber.vue'
+import Login from './pages/Login.vue'
 
 export default {
 	components: {
-		Welcome01,
-		SignUpOptions02,
-		SignUp03,
-		VerifyStep01,
-		VerifyStep02,
-		VerifyStep03,
-		Login04
+		Welcome,
+		SignUpOptions,
+		SignUp,
+		VerifyNumber,
+		Login
 	}
 }
 </script>
