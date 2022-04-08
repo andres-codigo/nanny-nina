05b4b1154e099c45ce1d4fb2cd54b1b94b8cb624
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-08 17:22:23 +1000

    [pages] existing event pages renamed to remove Event 'prefix'/Booking page created and DOM added

--- src/pages/03-host/nanny/pages/event/EventContainer.vue
+++ src/pages/03-host/nanny/pages/event/EventContainer.vue
@@ -1,18 +1,21 @@
 <template>
 	<div class="flex flex-wrap grow gap-y-6 gap-x-4">
-		<EventInformationForm class="max-w-450-for-dev-not-prod" />
-		<EventDescription class="max-w-450-for-dev-not-prod" />
+		<Information class="max-w-450-for-dev-not-prod" />
+		<Description class="max-w-450-for-dev-not-prod" />
+		<Booking class="max-w-450-for-dev-not-prod" />
 	</div>
 </template>
 
 <script>
-import EventInformationForm from './pages/EventInformationForm.vue'
-import EventDescription from './pages/EventDescription.vue'
+import Information from './pages/Information.vue'
+import Description from './pages/Description.vue'
+import Booking from './pages/Booking.vue'
 
 export default {
 	components: {
-		EventInformationForm,
-		EventDescription
+		Information,
+		Description,
+		Booking
 	}
 }
 </script>
