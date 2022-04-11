cfd3880e5b58a4fc511af9259850329e449888f9
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-11 12:02:22 +1000

    [components, pages] AnchorOutlineLight component created and button converted to anchor where applicable

---
+++ src/components/dom-elements/AnchorOutlineLight.vue
@@ -0,0 +1,34 @@
+<template>
+	<a
+		:href="url"
+		:target="hrefTarget"
+		:class="class"
+		class="
+			inline-flex
+			justify-center
+			rounded-full
+			font-bold
+			antialiased leading-none
+			border
+		">{{ text }}</a>
+</template>
+
+<script>
+export default {
+	props: {
+		url: {
+			type: String,
+			default: '#'
+		},
+		hrefTarget: {
+			type: String,
+			default: '_self'
+		},
+		class: {
+			type: String,
+			default: 'w-full text-xl py-4 text-white border-white hover:border-lilac-400 focus:border-green-500'
+		},
+		text: String
+	}
+}
+</script>
