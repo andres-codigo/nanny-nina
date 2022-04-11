d82f907f5a44e47f733c75f9ab31228719a2f72e
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-11 14:15:07 +1000

    [components, pages] UpdateAddress renamed VerifyAccount and DOM updated to match designs/width and height classes moved into inputClass prop on CheckboxLeftWithContainerAndLabelright component/TitleBooking page updated

---
+++ src/pages/03-host/nanny/pages/onboarding/VerifyAccount.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="w-full">
+		<TopNavigation text="Verify Account">
+			<template #iconLeft><router-link to="/dashboard"><ChevronLeftIcon class="w-8 h-8 text-white" aria-hidden="true" /></router-link></template>
+			<template #iconRight><QuestionMarkCircleIcon class="w-6 h-6 text-green-500" aria-hidden="true" /></template>
+		</TopNavigation>
+		<main>
+			<div class="h-full flex flex-col pt-12 mx-4">
+				<h5 class="text-left text-header-five font-semibold antialiased mb-2">Verify account</h5>
+				<p	class="text-sm font-light">We don't charge a fee for placing a booking but we require a mandatory €0.01  verification payment and a €35 monthly membership fee. This is to ensure that all our caregivers are paid for their services, you will only be charged if a match is found.</p>
+
+				<div class="flex flex-col justify-center items-center mt-40 mb-20">
+					<div class="grid grid-rows-2 gap-2">
+						<div><ButtonPrimary text="Verify" class="w-button-lg text-xl py-4" /></div>
+						<div><ButtonGhost text="Cancel booking" /></div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+import TopNavigation from '../../../../../components/navigation/TopNavigation.vue'
+import ButtonPrimary from '../../../../../components/buttons/ButtonPrimary.vue'
+import ButtonGhost from '../../../../../components/buttons/ButtonGhost.vue'
+
+import { ChevronLeftIcon, QuestionMarkCircleIcon } from '@heroicons/vue/solid'
+
+export default {
+	components: {
+		TopNavigation,
+		ButtonPrimary,
+		ButtonGhost,
+		ChevronLeftIcon,
+		QuestionMarkCircleIcon
+	},
+	props: {
+		title: String,
+		buttonText: String
+	}
+}
+</script>
