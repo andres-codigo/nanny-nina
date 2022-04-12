61a55505dc0d6edb62b2cd24e36b8d353d1548ee
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-12 12:27:20 +1000

    [components, pages] ChecklistItem and ChecklistItemContent components updated to include background colour prop for checklist item hover/focus colour states/ServiceOptions page updated/checklist item padding changed to mirror updated designs

--- src/components/checklist/ChecklistItem.vue
+++ src/components/checklist/ChecklistItem.vue
@@ -1,7 +1,7 @@
 <!-- TODO: bg color for default, hover & focus... overall look of page too -->
 <template>
 	<li>
-		<a href="#" target="_self" class="block w-full mb-4 border border-gray-100 bg-white hover:bg-gray-200 rounded-30px hover:rounded-30px" v-if="checkListItemIsHyperlink">
+		<a href="#" target="_self" :class="checklistItemBackgroundClasses" class="block w-full mb-4 border border-gray-100 bg-white rounded-30px hover:rounded-30px" v-if="checkListItemIsHyperlink">
 			<ChecklistItemContent
 				:hideStatusIcon="hideStatusIcon"
 				:hideContent="hideContent"
@@ -40,9 +40,37 @@
 import ChecklistItemContent from './ChecklistItemContent.vue'
 
 export default {
+	data() {
+    	return {
+			checklistItemBackgroundClasses: ''
+    	}
+  	},
 	components: {
 		ChecklistItemContent
 	},
-	props: ['checkListItemIsHyperlink', 'hideStatusIcon', 'hideMandatoryRedStar', 'hideContent', 'hidePending', 'title', 'description', 'showBadge', 'hideHyperlinkIcon']
+	props: [
+		'pageBackgroundColour',
+		'checkListItemIsHyperlink',
+		'hideStatusIcon',
+		'hideMandatoryRedStar',
+		'hideContent',
+		'hidePending',
+		'title',
+		'description',
+		'showBadge',
+		'hideHyperlinkIcon'
+	],
+	methods: {
+		checklistItemClasses(pageBackgroundColour) {
+			if (pageBackgroundColour === 'purple') {
+				this.checklistItemBackgroundClasses = 'hover:bg-green-400 hover:border-green-400 focus:bg-green-600 focus:border-green-600';
+			} else {
+				this.checklistItemBackgroundClasses = 'hover:bg-lilac-50 hover:border-lilac-50 focus:bg-lilac-100 focus:border-lilac-100';
+			}
+		}
+	},
+	mounted() {
+    	this.checklistItemClasses(this.pageBackgroundColour);
+  	}
 }
 </script>
