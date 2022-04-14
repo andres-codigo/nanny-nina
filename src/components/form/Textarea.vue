31c9c03ecedbf44b63106a67a99aa4a4e8748917
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-14 13:20:25 +1000

    [component] textarea component DOM/classes updated

--- src/components/form/Textarea.vue
+++ src/components/form/Textarea.vue
@@ -1,16 +1,19 @@
 <template>
-	<textarea
-		:rows="rows"
-		:name="name"
-		:id="id"
-		class="
-			block w-full
-			rounded-30px
-			border border-gray-100
-			text-gray-600
-			focus:outline-none focus:ring-0 focus:border-purple-500">
-	</textarea>
-	<div class="text-sm text-gray-500 text-right font-light">0/{{ characterNumber }}</div>
+	<div class="relative">
+		<textarea
+			:rows="rows"
+			:name="name"
+			:id="id"
+			maxlength="characterNumber"
+			class="
+				block w-full
+				rounded-30px
+				border border-gray-100
+				text-gray-600
+				focus:outline-none focus:ring-0 focus:border-purple-500">
+		</textarea>
+		<div class="absolute bottom-3 right-5 text-sm text-gray-500 text-right font-light">0/{{ characterNumber }}</div>
+	</div>
 </template>
 
 <script>
