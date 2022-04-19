21c5ae5aed0644e3ba1e61c717d4dfdf9e60a7bd
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-19 15:36:09 +1000

    [folders, components, pages] host nanny request overview merging of 'fold' content into single overview page requiring folder creation, file deletion and page updates

---
+++ src/components/profile/pricing/PricingConfirmationContainer.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<main>
+			<div class="h-full flex flex-col mx-4">
+				<PricingConfirmation />
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+import PricingConfirmation from './PricingConfirmation.vue'
+
+export default {
+	components: {
+		PricingConfirmation
+	}
+}
+</script>
