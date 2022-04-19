d02c9bb309e67c2914deb7f8a5dab268c130d9e0
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-19 11:59:26 +1000

    [components, pages] removed w-full class from FixedBottomContentAndButton from last commit and updated ParentProfileContainer as fixed bottom not applicable on this page

--- src/pages/02-caregiver/nanny/02-parent-profile/ParentProfileContainer.vue
+++ src/pages/02-caregiver/nanny/02-parent-profile/ParentProfileContainer.vue
@@ -2,7 +2,6 @@
 	<div class="flex flex-wrap grow gap-y-6 gap-x-4">
 		<ProfileVersion01 class="max-w-450-for-dev-not-prod" />
 		<ProfileVersion02 class="max-w-450-for-dev-not-prod" />
-		<FixedApplicationContent class="max-w-450-for-dev-not-prod" />
 		<ProfileReviews class="max-w-450-for-dev-not-prod" />
 		<AboutYou class="max-w-450-for-dev-not-prod" />
 		<Motivation class="max-w-450-for-dev-not-prod" />
@@ -13,7 +12,6 @@
 <script>
 import ProfileVersion01 from './pages/ProfileVersion01.vue'
 import ProfileVersion02 from './pages/ProfileVersion02.vue'
-import FixedApplicationContent from '../../../../components/profile/FixedApplicationContent.vue'
 import ProfileReviews from './pages/ProfileReviews.vue'
 import AboutYou from './pages/AboutYou.vue'
 import Motivation from './pages/Motivation.vue'
@@ -23,7 +21,6 @@ export default {
 	components: {
 		ProfileVersion01,
 		ProfileVersion02,
-		FixedApplicationContent,
 		ProfileReviews,
 		AboutYou,
 		Motivation,
