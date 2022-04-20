558b0a770c56a098991ed8ec5c1d4db92800ec62
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-21 08:54:10 +1000

    [pages] added InputFieldBorderBottomOnly import back into Background page

--- src/pages/02-caregiver/childminder/01-onboarding/pages/01-onboarding/Background.vue
+++ src/pages/02-caregiver/childminder/01-onboarding/pages/01-onboarding/Background.vue
@@ -121,6 +121,7 @@ import TopNavigation from '../../../../../../components/navigation/TopNavigation
 import RadioQuestion from '../../../../../../components/form/RadioQuestion.vue'
 import SelectMenu from '../../../../../../components/form/SelectMenu.vue'
 import Textarea from '../../../../../../components/form/Textarea.vue'
+import InputFieldBorderBottomOnly from '../../../../../../components/form/InputFieldBorderBottomOnly.vue'
 import MultiplePhotoUpload from '../../../../../../components/media/multiple-upload/MultiplePhotoUpload.vue'
 import ButtonOutlineDark from '../../../../../../components/buttons/ButtonOutlineDark.vue'
 import ButtonPrimary from '../../../../../../components/buttons/ButtonPrimary.vue'
@@ -133,6 +134,7 @@ export default {
 		RadioQuestion,
 		SelectMenu,
 		Textarea,
+		InputFieldBorderBottomOnly,
 		MultiplePhotoUpload,
 		ButtonOutlineDark,
 		ButtonPrimary,
