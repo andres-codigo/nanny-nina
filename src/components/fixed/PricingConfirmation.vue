b04441d776e7e69dfe8c6238e325a5fb5e3739f0
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-21 08:52:24 +1000

    [component] PricingConfirmation component updated based on design changes

--- src/components/fixed/PricingConfirmation.vue
+++ src/components/fixed/PricingConfirmation.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="flex flex-col">
-		<div class="flex bg-green-500 font-bold py-4 px-4">
-			<div class="basis-1/2">My Budget</div>
-			<div class="basis-1/2 items-center">
-				<span class="float-right">&euro;300-350</span>
+		<a href="#" class="flex bg-green-500 hover:bg-green-300 focus:bg-green-400 py-4 px-4">
+			<div class="inline-flex grow items-center font-bold">My Budget:<span class="pl-1">&euro;300-350</span></div>
+			<div class="grow-0 items-center">
+				<ChevronRightIcon class="w-8 h-8 text-purple-900" aria-hidden="true" />
 			</div>
-		</div>
+		</a>
 		<div class="grid grid-cols-2 gap-4 gap-x-2 py-6">
 			<div><ButtonGhost text="Cancel" /></div>
 			<div><PrimaryButton text="Complete" class="w-button-sm py-4" /></div>
@@ -17,10 +17,13 @@
 import ButtonGhost from '../buttons/ButtonGhost.vue'
 import PrimaryButton from '../buttons/ButtonPrimary.vue'
 
+import { ChevronRightIcon } from '@heroicons/vue/solid'
+
 export default {
 	components: {
 		ButtonGhost,
-		PrimaryButton
+		PrimaryButton,
+		ChevronRightIcon
 	}
 }
 </script>
