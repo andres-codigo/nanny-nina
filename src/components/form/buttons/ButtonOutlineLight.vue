3ca7bb092278966cf09497f62d90246f45de0642
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-21 12:16:50 +1000

    [components, pages] moved buttons folder into form folder as more applicable/updated all page references

---
+++ src/components/form/buttons/ButtonOutlineLight.vue
@@ -0,0 +1,25 @@
+<!-- TODO: bg color for default, hover & focus -->
+<template>
+	<button
+		type="submit"
+		:class="class"
+		class="
+			justify-center
+			rounded-full
+			font-bold
+			antialiased leading-none
+			border
+		">{{ text }}</button>
+</template>
+
+<script>
+export default {
+	props: {
+		class: {
+			type: String,
+			default: 'w-full text-xl py-4 text-white border-white hover:border-lilac-400 focus:border-green-500'
+		},
+		text: String
+	}
+}
+</script>
