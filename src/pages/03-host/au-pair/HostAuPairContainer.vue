7994d517b949f576284a02546def588af5773673
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-26 09:37:56 +1000

    [components, pages] Reference page and ReferenceCard component created and Container page updated

--- src/pages/03-host/au-pair/HostAuPairContainer.vue
+++ src/pages/03-host/au-pair/HostAuPairContainer.vue
@@ -1,17 +1,20 @@
 <template>
 	<div class="flex flex-wrap grow gap-y-6 gap-x-4">
-		<!-- <NewRequest class="max-w-450-for-dev-not-prod" /> -->
+		<Reference class="max-w-450-for-dev-not-prod" />
+		<NewRequest class="max-w-450-for-dev-not-prod" />
 		<PersonalityTest class="max-w-450-for-dev-not-prod" />
 	</div>
 </template>
 
 <script>
 import NewRequest from './pages/onboarding/NewRequest.vue'
+import Reference from './pages/onboarding/Reference.vue'
 import PersonalityTest from './pages/onboarding/PersonalityTest.vue'
 
 export default {
 	components: {
 		NewRequest,
+		Reference,
 		PersonalityTest
 	}
 }
