91dbd7b54a2d8723357c8338aa63ec2d6a43f606
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-04-28 16:43:33 +1000

    [folders, pages] Caregiver > Childminder > Booking folder and file set-up

---
+++ src/pages/02-caregiver/childminder/02-booking/BookingContainer.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="flex flex-wrap grow gap-y-6 gap-x-4">
+		<!-- <Name class="max-w-450-for-dev-not-prod" /> -->
+	</div>
+</template>
+
+<script>
+// import Name] from './pages/'
+
+// export default {
+// 	components: {
+
+// 	}
+// }
+</script>
