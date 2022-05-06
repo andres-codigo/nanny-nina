999157bfb3eabadcd117b9f538c59762de4d1ec3
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-05-06 11:59:29 +1000

    [pages] DOM/class updates on BookingMatch page to mirror design updates

--- src/pages/02-caregiver/nanny/03-booking/pages/BookingMatch.vue
+++ src/pages/02-caregiver/nanny/03-booking/pages/BookingMatch.vue
@@ -5,13 +5,13 @@
 		</TopNavigation>
 		<main>
 			<div class="h-full first-letter:flex flex-col items-center justify-center pt-12 mx-4">
-				<div>
+				<div class="flex flex-col pb-8">
 					<h2 class="font-semibold text-center text-header-two text-green-500 antialiased">Yay!</h2>
-					<h2 class="font-semibold text-center text-header-two text-green-500 antialiased mx-6">You have matched with Brittany</h2>
-					<p class="font-medium antialiased text-white text-center pt-4">Brittany has now been added to your chat</p>
+					<h2 class="font-semibold text-center text-header-two text-green-500 antialiased mx-10">You have matched with The Sanders</h2>
+					<p class="font-medium antialiased text-white text-center pt-4 mx-20">The Sanders have now been added to your chat</p>
 				</div>
 
-				<div class="flex flex-col justify-center items-center mt-24">
+				<div class="flex flex-col justify-center items-center pb-12">
 					<div class="inline-flex -space-x-3">
 						<div><img class="rounded-full ring-offset-4 ring-0 h-32 w-32 z-0" src="https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt=""></div>
 						<div><img class="rounded-full ring-offset-4 ring-0 h-32 w-32 z-10" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt=""></div>
@@ -19,9 +19,9 @@
 				</div>
 
 				<div class="flex flex-col justify-center items-center mt-24 mb-10">
-					<p class="font-medium antialiased text-white text-center pb-10 mx-4	">To view her work hours or make payment to Brittany go to your matches in your booking page</p>
+					<p class="font-medium antialiased text-white text-center pb-10 mx-8	">View and update your work hours under Matches in your booking page</p>
 					<div class="grid grid-rows-2 gap-2">
-						<div><ButtonSecondary text="Contact Brittany" class="w-button-xl text-xl py-4" /></div>
+						<div><ButtonSecondary text="Contact The Sanders" class="w-button-xl text-xl py-4" /></div>
 						<div><ButtonOutlineLight text="Go to Matches" class="w-button-xl text-xl py-4 text-white border-white hover:border-lilac-400 focus:border-green-500" /></div>
 					</div>
 				</div>
