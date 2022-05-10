06e449fbba0207ecdf8a45471baf9d0efd450797
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-05-10 14:01:58 +1000

    [components, pages] CheckboxRightWithContainerAndLabelLeft component font size class updated/ReasonForCancellation page updated

--- src/pages/02-caregiver/nanny/04-cancel/pages/ReasonForCancellation.vue
+++ src/pages/02-caregiver/nanny/04-cancel/pages/ReasonForCancellation.vue
@@ -7,27 +7,19 @@
 			<div class="flex flex-col py-6 mx-4">
 				<form class="w-full" action="#" method="POST">
 					<div class="mb-10">
-						<h5 class="text-header-five font-semibold antialiased mb-4">Reason for cancellation?</h5>
-						<p class="font-thin mb-16">Your feedback is important to us to be able to better improve our service</p>
+						<h4 class="text-header-four font-semibold antialiased mb-4">Reason for cancellation?</h4>
+						<p class="font-thin">Your feedback is important to us to be able to better improve our service</p>
 
-						<RadioQuestionsInputLeftLabelRight
-							:screenReaderOnly="true"
-							:sideBySideRadioButtons="false"
-							:isQuestionOne="true"
-							questionOneText="I found another alternative"
-							questionTwoText="I no longer want to be a nanny"
-							:hasThirdQuestion="true"
-							questionThreeText="It took too long to find a family"
-							:hasFourthQuestion="true"
-							questionFourText="I could not resolve an issue"
-							:hasFifthQuestion="true"
-							questionFiveText="Other"
-						/>
+						<div class="mt-4">
+							<CheckboxRightWithContainerAndLabelLeft checkboxName="checkout-1" text="Sick/Unwell" />
+							<CheckboxRightWithContainerAndLabelLeft checkboxName="checkout-2" text="I no longer want to be a nanny" />
+							<CheckboxRightWithContainerAndLabelLeft checkboxName="checkout-3" text="Our personalities did not fit" />
+							<CheckboxRightWithContainerAndLabelLeft checkboxName="checkout-4" text="I can no longer be available for schedule" />
+						</div>
 					</div>
 
 					<div class="mb-20">
 						<p class="font-thin">Please explain further</p>
-
 						<InputFieldBorderBottomOnly type="text" />
 					</div>
 
@@ -42,7 +34,7 @@
 
 <script>
 import TopNavigation from '../../../../../components/navigation-and-tabs/TopNavigation.vue'
-import RadioQuestionsInputLeftLabelRight from '../../../../../components/form/RadioQuestionsInputLeftLabelRight.vue'
+import CheckboxRightWithContainerAndLabelLeft from '../../../../../components/form/CheckboxRightWithContainerAndLabelLeft.vue'
 import InputFieldBorderBottomOnly from '../../../../../components/form/InputFieldBorderBottomOnly.vue'
 import ButtonPrimary from '../../../../../components/buttons/ButtonPrimary.vue'
 
@@ -51,7 +43,7 @@ import { XIcon } from '@heroicons/vue/solid'
 export default {
 	components: {
 		TopNavigation,
-		RadioQuestionsInputLeftLabelRight,
+		CheckboxRightWithContainerAndLabelLeft,
 		InputFieldBorderBottomOnly,
 		ButtonPrimary,
 		XIcon,
