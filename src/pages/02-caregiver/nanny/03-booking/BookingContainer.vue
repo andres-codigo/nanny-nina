f18a145fd885d8d5074a834dc96d82dde39445a2
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-05-10 12:59:59 +1000

    [components, pages, svg] Review page created and added to Container page/purple star (outline and solid) svg's created and gold svg star renamed

--- src/pages/02-caregiver/nanny/03-booking/BookingContainer.vue
+++ src/pages/02-caregiver/nanny/03-booking/BookingContainer.vue
@@ -1,11 +1,12 @@
 <template>
 	<div class="flex flex-wrap grow gap-y-6 gap-x-4">
-		<InvitesNoContent class="max-w-450-for-dev-not-prod" />
+		<!-- <InvitesNoContent class="max-w-450-for-dev-not-prod" />
 		<InvitesWithContent class="max-w-450-for-dev-not-prod" />
 		<BookingMatch class="max-w-450-for-dev-not-prod" />
 		<MatchesWithContentVersionOne class="max-w-450-for-dev-not-prod" />
 		<MatchesWithContentVersionTwo class="max-w-450-for-dev-not-prod" />
-		<WorkingHours class="max-w-450-for-dev-not-prod"  />
+		<WorkingHours class="max-w-450-for-dev-not-prod"  /> -->
+		<Review class="max-w-450-for-dev-not-prod"  />
 	</div>
 </template>
 
@@ -16,6 +17,7 @@ import BookingMatch from './pages/BookingMatch.vue'
 import MatchesWithContentVersionOne from './pages/MatchesWithContentVersionOne.vue'
 import MatchesWithContentVersionTwo from './pages/MatchesWithContentVersionTwo.vue'
 import WorkingHours from './pages/WorkingHours.vue'
+import Review from './pages/Review.vue'
 
 export default {
 	components: {
@@ -24,7 +26,8 @@ export default {
 		BookingMatch,
 		MatchesWithContentVersionOne,
 		MatchesWithContentVersionTwo,
-		WorkingHours
+		WorkingHours,
+		Review
 	}
 }
 </script>
---
+++ src/pages/02-caregiver/nanny/03-booking/pages/Review.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="w-full h-full">
+		<TopNavigation backgroundColor="bg-white-500" :dropShadow="false">
+			<template #iconRight><router-link to="/dashboard"><XIcon class="w-8 h-8 text-purple-900" aria-hidden="true" /></router-link></template>
+		</TopNavigation>
+		<main>
+			<div class="flex flex-col pt-12 px-4">
+				<form class="w-full" action="#" method="POST">
+					<div class="flex flex-col mb-4">
+						<h5 class="inline-flex items-baseline text-header-five font-semibold antialiased mb-2">The Sanders<span class="text-4xl mx-1">4/5</span><Img src="./assets/svg/other/star-solid-gold.svg" /></h5>
+						<p class="font-sm">What did you think of The Sanders?</p>
+					</div>
+
+					<div class="mb-10">
+						<p class="text-sm text-gray-500 mb-3">Tap a start to rate</p>
+						<div class="grid grid-cols-5 gap-2">
+							<div>
+								<Img src="./assets/svg/other/star-solid-purple.svg" class="w-12 h-12 mb-2" />
+								<span class="inline-flex w-12 text-sm justify-center">Bad</span>
+							</div>
+							<div><Img src="./assets/svg/other/star-solid-purple.svg" class="w-12 h-12" /></div>
+							<div><Img src="./assets/svg/other/star-outline-purple.svg" class="w-12 h-12" /></div>
+							<div><Img src="./assets/svg/other/star-outline-purple.svg" class="w-12 h-12" /></div>
+							<div>
+								<Img src="./assets/svg/other/star-outline-purple.svg" class="w-12 h-12 mb-2" />
+								<span class="inline-flex w-12 text-sm justify-center">Good</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="mb-20">
+						<label for="review" class="sr-only">Leave a review</label>
+						<p	class="text-sm font-light text-gray-500">Leave a review</p>
+						<div class="mt-8">
+							<Textarea name="review" id="review" />
+						</div>
+					</div>
+
+					<div class="flex justify-center mb-20">
+						<ButtonPrimary text="Send" class="w-button-lg text-xl py-4" />
+					</div>
+				</form>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+import TopNavigation from '../../../../../components/navigation-and-tabs/TopNavigation.vue'
+import Img from '../../../../../components/dom-elements/Img.vue'
+import Textarea from '../../../../../components/form/Textarea.vue'
+import ButtonPrimary from '../../../../../components/buttons/ButtonPrimary.vue'
+
+import { XIcon } from '@heroicons/vue/solid'
+
+export default {
+	components: {
+		TopNavigation,
+		Img,
+		Textarea,
+		ButtonPrimary,
+		XIcon
+	}
+}
+</script>
