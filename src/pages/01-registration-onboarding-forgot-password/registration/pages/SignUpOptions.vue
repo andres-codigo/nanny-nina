4b4307bb93f03b8799a2827dd17939c74ce5f186
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-05-12 16:54:38 +1000

    [components, pages, folders] ResponseCard component created/ResponsesWithContent page created and DOM based off designs added/updated default, hover and focus hex values based off system design updates/BadgeBookingType component updated

--- src/pages/01-registration-onboarding-forgot-password/registration/pages/SignUpOptions.vue
+++ src/pages/01-registration-onboarding-forgot-password/registration/pages/SignUpOptions.vue
@@ -8,12 +8,12 @@
 			<div class="min-h-screen flex flex-col justify-center pt-12 mx-4">
 				<h5 class="text-header-five font-semibold antialiased mb-2">Welcome to Nanny Nina</h5>
 				<div class="inline-flex pb-8">
-					<LinkCard to="dashboard" class="text-purple-900 bg-green-500 hover:bg-green-300 focus:bg-green-400" title="Sign up as Parent/Host" >
+					<LinkCard to="dashboard" class="text-purple-900 bg-green-500 hover:bg-green-400 focus:bg-green-600" title="Sign up as Parent/Host" >
 						<template #details><p class="pt-2">Request Nanny, Childminder or Au pair</p></template>
 					</LinkCard>
 				</div>
 				<div class="inline-flex pb-8">
-					<LinkCard to="dashboard" class="text-white bg-purple-500 hover:bg-purple-300 focus:bg-purple-400" title="Sign up for Care jobs">
+					<LinkCard to="dashboard" class="text-white bg-purple-500 hover:bg-purple-300 focus:bg-purple-600" title="Sign up for Care jobs">
 						<template #details><p class="text-white pt-2">Work as Nanny, Childminder or Au pair</p></template>
 					</LinkCard>
 				</div>
--- src/pages/03-host/nanny/01-onboarding/pages/onboarding/NewRequest.vue
+++ src/pages/03-host/nanny/01-onboarding/pages/onboarding/NewRequest.vue
@@ -7,12 +7,12 @@
 			<div class="flex flex-col justify-center pt-12 mx-4">
 				<h5 class="text-header-five font-semibold antialiased mb-4">What kind of nanny do you want?</h5>
 				<div class="inline-flex pb-8">
-					<LinkCard  :showPlaceholderSVG="true" to="dashboard" class="text-white bg-purple-500 hover:bg-purple-300 focus:bg-purple-400" title="One-time">
+					<LinkCard  :showPlaceholderSVG="true" to="dashboard" class="text-white bg-purple-500 hover:bg-purple-300 focus:bg-purple-600" title="One-time">
 						<template #details><p class="text-white pt-2">Urgent nanny for single or multiple days. Maximum 12days </p></template>
 					</LinkCard>
 				</div>
 				<div class="inline-flex pb-8">
-					<LinkCard to="dashboard" class="text-purple-900 bg-green-500 hover:bg-green-300 focus:bg-green-400" title="Recurring" >
+					<LinkCard to="dashboard" class="text-purple-900 bg-green-500 hover:bg-green-400 focus:bg-green-600" title="Recurring" >
 						<template #details><p class="pt-2 pr-24">I need a nanny on returning days over a longer period</p></template>
 					</LinkCard>
 				</div>
