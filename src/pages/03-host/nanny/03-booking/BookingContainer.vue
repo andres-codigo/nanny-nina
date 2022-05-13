78a52560f6ee54b1bb8767321032d832b34a9642
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-05-13 14:05:53 +1000

    [pages] reordered display of pages on Container page

--- src/pages/03-host/nanny/03-booking/BookingContainer.vue
+++ src/pages/03-host/nanny/03-booking/BookingContainer.vue
@@ -1,21 +1,21 @@
 <template>
 	<div class="flex flex-wrap grow gap-y-6 gap-x-4">
 		<ResponsesWithContent class="max-w-450-for-dev-not-prod" />
-		<ProfileBadges class="max-w-450-for-dev-not-prod" />
 		<Availability class="max-w-450-for-dev-not-prod" />
+		<ProfileBadges class="max-w-450-for-dev-not-prod" />
 	</div>
 </template>
 
 <script>
 import ResponsesWithContent from './pages/ResponsesWithContent.vue'
-import ProfileBadges from './pages/ProfileBadges.vue'
 import Availability from './pages/Availability.vue'
+import ProfileBadges from './pages/ProfileBadges.vue'
 
 export default {
 	components: {
 		ResponsesWithContent,
-		ProfileBadges,
-		Availability
+		Availability,
+		ProfileBadges
 	}
 }
 </script>
