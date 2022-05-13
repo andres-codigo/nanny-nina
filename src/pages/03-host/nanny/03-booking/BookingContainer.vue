77fcbc79adff2d1a2d72caaa819bbfa7e0834be7
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-05-13 14:05:01 +1000

    [pages] Availability page created and DOM added and added to Container page

--- src/pages/03-host/nanny/03-booking/BookingContainer.vue
+++ src/pages/03-host/nanny/03-booking/BookingContainer.vue
@@ -2,17 +2,20 @@
 	<div class="flex flex-wrap grow gap-y-6 gap-x-4">
 		<ResponsesWithContent class="max-w-450-for-dev-not-prod" />
 		<ProfileBadges class="max-w-450-for-dev-not-prod" />
+		<Availability class="max-w-450-for-dev-not-prod" />
 	</div>
 </template>
 
 <script>
 import ResponsesWithContent from './pages/ResponsesWithContent.vue'
 import ProfileBadges from './pages/ProfileBadges.vue'
+import Availability from './pages/Availability.vue'
 
 export default {
 	components: {
 		ResponsesWithContent,
-		ProfileBadges
+		ProfileBadges,
+		Availability
 	}
 }
 </script>
