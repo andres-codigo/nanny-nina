e62998c40541cb3ebd01997af6a8fc1c024d774c
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-05-13 13:52:35 +1000

    [pages] profile details DOM update to resolve small device alignment fix for long content

--- src/pages/03-host/nanny/03-booking/pages/NannyProfile.vue
+++ src/pages/03-host/nanny/03-booking/pages/NannyProfile.vue
@@ -16,7 +16,7 @@
 				<div class="mx-4">
 					<h5 class="text-header-five font-semibold antialiased mt-4 mb-2">Meet Brittany</h5>
 
-					<div class="flex flex-row font-thin">
+					<div class="flex flex-wrap gap-0 gap-y-2 mt-4">
 						<div class="inline-flex items-center">
 							<Img src="./assets/svg/other/star-solid-gold.svg" class="w-5 h-5" />
 							<span class="pl-1 text-sm font-semibold">(4.5/5)</span>
@@ -25,11 +25,13 @@
 						<div class="inline-flex items-center">
 							<Img src="./assets/svg/other/cake.svg" class="w-5 h-5" />
 							<span class="pl-1 text-sm">20th September</span>
-							<span class="h-1 w-1 bg-gray-500 rounded-full mx-1"></span>
-							<span class="pl-1 text-sm">27 yrs</span>
-							<span class="h-1 w-1 bg-gray-500 rounded-full mx-1"></span>
 						</div>
 						<div class="inline-flex items-center">
+							<span class="h-1 w-1 bg-gray-500 rounded-full mx-1"></span>
+							<span class="pl-1 text-sm">27 yrs</span>
+						</div>
+						<div class="inline-flex items-center">
+							<span class="h-1 w-1 bg-gray-500 rounded-full mx-1"></span>
 							<Img src="./assets/svg/other/medal-gold.svg" class="w-5 h-5" />
 							<span class="pl-1 text-sm">Creative</span>
 						</div>
