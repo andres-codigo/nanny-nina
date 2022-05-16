920bb7ce4503e3d34a7a8f16c6e694cc1919e961
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-05-16 14:52:24 +1000

    [pages] InvitesNoContent page updated to relect design change

--- src/pages/02-caregiver/nanny/03-booking/pages/InvitesNoContent.vue
+++ src/pages/02-caregiver/nanny/03-booking/pages/InvitesNoContent.vue
@@ -18,18 +18,19 @@
 					:tabThreeIsActive="false"
 				/>
 
-				<div class="flex flex-row pt-6 mx-4">
-					<div class="basis-1/2">
-						<h4 class="text-header-four font-semibold antialiased">Your Invites</h4>
-					</div>
-					<div class="basis-1/2">
-						<div class="inline-flex float-right items-center"><Img src="./assets/svg/other/clock-outline.svg" /><a href="#" target="_self" class="anchor-blue anchor-with-underline ml-1">History</a></div>
+				<div class="pt-6 mx-4">
+					<h4 class="text-header-four font-semibold antialiased">Your Invites</h4>
+				</div>
+
+				<div class="flex flex-col items-center mt-44">
+					<div class="grid grid-rows-2 gap-0">
+						<div class="font-thin text-gray-500">You have no invites yet.</div>
+						<div><AnchorPrimary text="Start Applying" class="w-button-sm py-4" /></div>
 					</div>
 				</div>
 
-				<div class="mt-44 mx-20 font-thin text-center">
-					<p class="text-gray-500 mb-32">You have no invites yet. <a href="#" target="_blank" class="anchor-blue anchor-with-underline">Apply to vacancies</a></p>
-					<p class="inline-flex items-center text-gray-500"><LightBulbIcon class="w-5 h-5" />Tip: <a href="#" target="_self" class="anchor-blue anchor-with-underline ml-1">Add more pictures</a></p>
+				<div class="mt-44 text-center">
+					<p class="inline-flex items-center text-gray-500 font-thin"><LightBulbIcon class="w-5 h-5" />Tip: <a href="#" target="_self" class="anchor-blue anchor-with-underline ml-1">Add more pictures</a></p>
 				</div>
 			</div>
 		</main>
@@ -39,7 +40,7 @@
 <script>
 import TopNavigation from '../../../../../components/navigation-and-tabs/TopNavigation.vue'
 import Tabs from '../../../../../components/navigation-and-tabs/Tabs.vue'
-import Img from '../../../../../components/dom-elements/Img.vue'
+import AnchorPrimary from '../../../../../components/dom-elements/AnchorPrimary.vue'
 
 import { QuestionMarkCircleIcon, ClockIcon, LightBulbIcon } from '@heroicons/vue/solid'
 
@@ -47,7 +48,7 @@ export default {
 	components: {
 		TopNavigation,
 		Tabs,
-		Img,
+		AnchorPrimary,
 		QuestionMarkCircleIcon,
 		ClockIcon,
 		LightBulbIcon
