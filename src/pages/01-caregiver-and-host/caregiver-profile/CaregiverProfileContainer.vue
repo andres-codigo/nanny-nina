a3aec7bf3f974711b1dd2cf794ce04cb181e5dde
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-06-15 16:50:23 +1000

    [pages] Personal (WIP) page created and DOM added and added to container page

--- src/pages/01-caregiver-and-host/caregiver-profile/CaregiverProfileContainer.vue
+++ src/pages/01-caregiver-and-host/caregiver-profile/CaregiverProfileContainer.vue
@@ -1,15 +1,18 @@
 <template>
 	<div class="flex flex-wrap grow gap-y-6 gap-x-4">
 		<Profile class="max-w-450-for-dev-not-prod" />
+		<Personal class="max-w-450-for-dev-not-prod" />
 	</div>
 </template>
 
 <script>
 import Profile from './pages/Profile.vue'
+import Personal from './pages/Personal.vue'
 
 export default {
 	components: {
 		Profile,
+		Personal
 	}
 }
 </script>
---
+++ src/pages/01-caregiver-and-host/caregiver-profile/pages/Personal.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="w-full h-full">
+		<TopNavigation text="Profile">
+			<template #iconLeft><router-link to="'/dashboard?tab=1"><ChevronLeftIcon class="w-8 h-8 text-white" aria-hidden="true" /></router-link></template>
+			<template #iconRight><QuestionMarkCircleIcon class="w-6 h-6 text-green-500" aria-hidden="true" /></template>
+		</TopNavigation>
+		<main>
+			<Tabs
+				tabOneClasses="text-purple-900"
+				tabOneText="Personal"
+				:tabOneIsActive="true"
+				tabTwoClasses="border-transparent text-gray-500 border-b-2 hover:text-gray-700 hover:border-gray-300"
+				tabTwoText="Preference"
+				:tabTwoIsActive="false"
+				:displayThirdTab="true"
+				tabThreeClasses="border-transparent text-gray-500 border-b-2 hover:text-gray-700 hover:border-gray-300"
+				tabThreeText="Badges"
+				:tabThreeIsActive="false"
+			/>
+
+			<div class="flex flex-col mx-4">
+				<div class="flex flex-col justify-center items-center pt-12">
+					<Img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" class="h-44 w-44 rounded-full" />
+				</div>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+import TopNavigation from '../../../../components/navigation-and-tabs/TopNavigation.vue'
+import Tabs from '../../../../components/navigation-and-tabs/Tabs.vue'
+import Img from '../../../../components/dom-elements/Img.vue'
+
+import { ChevronLeftIcon, QuestionMarkCircleIcon } from '@heroicons/vue/solid'
+
+export default {
+	components: {
+		TopNavigation,
+		Tabs,
+		Img,
+		ChevronLeftIcon,
+		QuestionMarkCircleIcon
+	}
+}
+</script>
