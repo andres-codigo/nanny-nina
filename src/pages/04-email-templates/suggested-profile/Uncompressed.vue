ba6c486bc4da16ca1b774d97e8b86132f38178d6
andres-cuenta <52874360+solaris-estrella@users.noreply.github.com>
2022-06-22 09:40:23 +1000

    [vue, html, js] converstion of html files to vue to resolve rendering vercel app/dashboard items added and static anchor tags removed/html to vue templates updated to hide html, meta and body tags

--- src/pages/04-email-templates/suggested-profile/Uncompressed.vue
+++ src/pages/04-email-templates/suggested-profile/Uncompressed.vue
@@ -1,4 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<template>
+	<!-- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -6,7 +7,7 @@
 <meta name="color-scheme" content="light">
 <meta name="supported-color-schemes" content="light">
 </head>
-<body style="font-family: Arial, Helvetica, sans-serif; box-sizing: border-box; height: 100%; hyphens: auto; line-height: 1.4; margin: 0; -moz-hyphens: auto; -ms-word-break: break-all; width: 100% !important; -webkit-hyphens: auto; -webkit-text-size-adjust: none; word-break: break-word;">
+<body style="font-family: Arial, Helvetica, sans-serif; box-sizing: border-box; height: 100%; hyphens: auto; line-height: 1.4; margin: 0; -moz-hyphens: auto; -ms-word-break: break-all; width: 100% !important; -webkit-hyphens: auto; -webkit-text-size-adjust: none; word-break: break-word;"> -->
 	<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation" style="font-family: Arial, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
 		<tr>
 			<td align="center" style="font-family: Arial, Helvetica, sans-serif; box-sizing: border-box;">
@@ -283,5 +284,6 @@
 			</td>
 		</tr>
 	</table>
-</body>
-</html>
+<!--</body>
+ </html> -->
+ </template>
--- src/router/index.js
+++ src/router/index.js
@@ -74,7 +74,15 @@ import Dashboard from '../pages/_dashboard/Dashboard.vue'
 
 /* END: Host - Childminder Nanny Au Pair */
 
-	import Email from '../pages/04-email-templates/email-verification/Uncompressed.vue'
+/* START: Email Templates */
+
+	import EmailVerificationTemplate from '../pages/04-email-templates/email-verification/Uncompressed.vue'
+	import EmailPasswordResetTemplate from '../pages/04-email-templates/password-reset/Uncompressed.vue'
+	import EmailPaymentReceivedTemplate from '../pages/04-email-templates/payment-received/Uncompressed.vue'
+	import EmailPaymentSuccessfulTemplate from '../pages/04-email-templates/payment-successful/Uncompressed.vue'
+	import EmailSuggestedProfileTemplate from '../pages/04-email-templates/suggested-profile/Uncompressed.vue'
+
+/* END: Email Templates */
 
 const routes = [
 	{
@@ -96,11 +104,7 @@ const routes = [
 			}
 		}
 	},
-{
-		path: '/email',
-		component: Email,
-		meta: { transition: 'slide-right-to-left' }
-	},
+
 	/*
 		Caregiver and Host
 	*/
@@ -328,7 +332,36 @@ const routes = [
 		path: '/host-au-pair',
 		component: HostAuPairContainer,
 		meta: { transition: 'slide-right-to-left' }
-	}
+	},
+
+	/*
+		Email Templates
+	*/
+	{
+		path: '/email-verification',
+		component: EmailVerificationTemplate,
+		meta: { transition: 'slide-right-to-left' }
+	},
+	{
+		path: '/email-password-reset',
+		component: EmailPasswordResetTemplate,
+		meta: { transition: 'slide-right-to-left' }
+	},
+	{
+		path: '/email-payment-received',
+		component: EmailPaymentReceivedTemplate,
+		meta: { transition: 'slide-right-to-left' }
+	},
+	{
+		path: '/email-payment-successful',
+		component: EmailPaymentSuccessfulTemplate,
+		meta: { transition: 'slide-right-to-left' }
+	},
+	{
+		path: '/email-suggested-profile',
+		component: EmailSuggestedProfileTemplate,
+		meta: { transition: 'slide-right-to-left' }
+	},
 ]
 const router = createRouter({
 	history: createWebHistory(),
