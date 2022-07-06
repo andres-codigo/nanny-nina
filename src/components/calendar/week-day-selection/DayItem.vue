f403c8240384d7e4539854af66bb1577a448aed1
solaris-estrella <52874360+solaris-estrella@users.noreply.github.com>
2022-07-06 13:44:35 +1000

    [components, pages] week day selection components created/WeeklySchedule page created and WIP DOM added

---
+++ src/components/calendar/week-day-selection/DayItem.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="flex flex-nowrap">
+		<span class="
+			inline-flex justify-center items-center
+			rounded-full
+			text-purple-900 font-bold leading-none
+			border-0
+			cursor-pointer
+			w-10 h-10
+			mx-2 py-1
+			hover:bg-gray-50
+			focus:bg-gray-100
+			">
+			<span class="inline-flex text-xs">{{ text }}</span>
+		</span>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		text: String
+	}
+}
+</script>
---
+++ src/components/calendar/week-day-selection/Week.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="flex border-b border-b-1">
+		<div class="inline-flex grow overflow-x-scroll py-4 no-scrollbar items-center">
+			<slot name="one"></slot>
+			<slot name="two"></slot>
+			<slot name="three"></slot>
+			<slot name="four"></slot>
+			<slot name="five"></slot>
+			<slot name="six"></slot>
+			<slot name="seven"></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+import Img from '../../dom-elements/Img.vue'
+
+import { AdjustmentsIcon } from '@heroicons/vue/solid'
+
+export default {
+	components: {
+		Img,
+		AdjustmentsIcon
+	}
+}
+</script>
